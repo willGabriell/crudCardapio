82813d641d96005ff7e462888dfb33f10cc7d16e
Wilinton Gabriel <wilintongabrielss1@gmail.com>
2024-11-19 10:23:05 -0300

    configurando projeto

---
+++ src/main/java/com/crudcardapio/CrudCardapioApplication.java
@@ -0,0 +1,13 @@
+package com.crudcardapio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudCardapioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudCardapioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/crudcardapio/CrudCardapioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crudcardapio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudCardapioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
